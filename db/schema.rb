73199e52d845f2acd303f925e495a88a9db3f183
Aryan Pal <aryanpalongraph@gmail.com>
2022-07-15 11:04:44 +0530

    📝 Added Association in Comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_14_124400) do
+ActiveRecord::Schema.define(version: 2022_07_15_052211) do
 
   create_table "comments", force: :cascade do |t|
     t.string "commenter_name"
     t.string "comment"
+    t.integer "user_id"
+    t.integer "post_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -24,7 +26,7 @@ ActiveRecord::Schema.define(version: 2022_07_14_124400) do
     t.string "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
+    t.integer "user_id" 
   end
 
   create_table "users", force: :cascade do |t|
